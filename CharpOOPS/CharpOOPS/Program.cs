2b6db84eafea3112559ad488279736b3304047de
Sallagunta <sallaguntagowthami@gmail.com>
2022-01-01 22:07:10 +0530

    updated..

--- CharpOOPS/CharpOOPS/Program.cs
+++ CharpOOPS/CharpOOPS/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,25 @@ namespace CharpOOPS
     {
         static void Main(string[] args)
         {
-           
+            
+            Eligible();
+        }
+
+        public static void Eligible()
+        {
+            CollectionsDemo collectionsDemo = new CollectionsDemo();
+            List<int> getAgeList = collectionsDemo.GetAge(1997);
+            for (int i = 0; i < getAgeList.Count; i++)
+            {
+
+            }
+            int age =getAgeList[0];
+            if (age > 18 && age < 28)
+                Console.WriteLine("Class 1 type");
+            else if (age > 28 && age < 38)
+                Console.WriteLine("Class 2 type");
+            else if (age > 38 && age < 28)
+                Console.WriteLine("Class 3 type");
         }
     }
 }
